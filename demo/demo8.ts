977c61ee9aa0c92f1473cfeaef08a12cd5fa86c2
Rick Xu <48512796+xuxuejin@users.noreply.github.com>
2021-07-09 01:11:30 +0800

    Update demo8.ts

--- demo/demo8.ts
+++ demo/demo8.ts
@@ -11,8 +11,10 @@ class Teacher extends Person {
   getTeacherName() {
     return 'Teacher';
   }
+  // 类重写
   getName() {
-    //
+    // super 可以理解为指代父类
+    // 把父类的方法重写了，这时候还想调用父类的方法，就可以 super
     return super.getName + 'Xu';
   }
 }
